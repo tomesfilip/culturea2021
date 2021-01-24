28ff017654f5f2adc5d2853867f170e91c116707
tomesfilip <filiptomes@protonmail.com>
2021-01-24 20:33:33 +0100

    implemented mapbox

--- src/components/Mapbox.vue
+++ src/components/Mapbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="map" class="basemap"></div>
+  <div id="mapContainer" class="basemap"></div>
 </template>
 
 
@@ -20,23 +20,41 @@ export default {
     }
   },
   methods: {
+    // initMap() {
+    //   this.map = L.map('map').setView([49.22645, 17.67065], 20);
+    //   this.tileLayer = L.tileLayer(
+    //     'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+    //     {
+    //       maxZoom: 22,
+    //       attribution: 'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+
+    //     }
+    //   );
+    //   this.tileLayer.addTo(this.map);
+    // },
+    // initLayers() {},
     initMap() {
-      this.map = L.map('map').setView([49.22645, 17.67065], 20);
-      this.tileLayer = L.tileLayer(
-        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
-        {
-          maxZoom: 22,
-          attribution: 'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-          
-        }
-      );
-      this.tileLayer.addTo(this.map);
-    },
-    initLayers() {},
+      mapboxgl.accessToken = this.accessToken
+
+      this.map = new mapboxgl.Map({
+        container: "mapContainer",
+        style: "mapbox://styles/f-tomes/ckkb87n7u0nq917nyabyybt4l",
+        center: [17.674, 49.225],
+        zoom: 13,
+        minZoom: 13,
+        maxZoom: 18,
+        maxBounds: [
+          [17.3, 49.0],
+          [17.9, 49.3],
+        ],
+      })
+    }
   },
   mounted() {
     this.initMap()
-    this.initLayers()
+
+    const nav = new mapboxgl.NavigationControl();
+    this.map.addControl(nav, "top-right");
   }
   
 }
@@ -45,7 +63,7 @@ export default {
 
 <style>
   .basemap {
-    width: 100vw;
+    width: 100%;
     height: 100vh;
   }
 </style>
