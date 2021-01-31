5267423250e37caf5b4abb6f0ad4d2f9e3083b12
tomesfilip <filiptomes@protonmail.com>
2021-01-31 19:43:14 +0100

    heremap component deleted

--- src/views/MainMap.vue
+++ src/views/MainMap.vue
@@ -3,11 +3,10 @@
 </template>
 
 <script>
-import HereMap from '../components/HereMap.vue'
 import Mapbox from '../components/Mapbox.vue'
 
 export default {
-  components: { HereMap, Mapbox }
+  components: { Mapbox }
 }
 </script>
 
