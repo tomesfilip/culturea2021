bd35eb1b01e506fd5ac8d02086c5436e6656e2ca
tomesfilip <filiptomes@protonmail.com>
2021-02-07 21:05:20 +0100

    partner logos

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,28 +1,42 @@
 <template>
   <h2 class="uppercase text-4xl mb-16 text-center font-heading">Partneři</h2>
   <Container class="lg:px-16 px-4">
-    <div class="logos grid xl:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-4">
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
-      <div class="bg-gray-300"></div>
+    <div class="logos grid xl:grid-cols-4 md:grid-cols-3 mobile:grid-cols-2 grid-cols-1 gap-4 items-center">
+      <img v-for="(logo, logoName) in logos" :key="logoName" :src="logo.logoImg" :alt="logo.logoName">
     </div>
-  </Container>
-  
+  </Container>  
 </template>
 
 <script>
 import Container from './Container.vue'
+
+import dachmediaImg from '../assets/img/partners/dachmedia.png'
+import stamgastImg from '../assets/img/partners/stamgast_gurman.png'
+import vychytaneImg from '../assets/img/partners/vychytane_cz.png'
+import zlinImg from '../assets/img/partners/zlin_cz.png'
+
 export default {
   components: { Container },
   data() {
     return {
+      logos: [
+        {
+          logoName: 'dachmedia',
+          logoImg: dachmediaImg
+        },
+        {
+          logoName: 'Štamgast & Gurmán',
+          logoImg: stamgastImg
+        },
+        {
+          logoName: 'Vychytané',
+          logoImg: vychytaneImg
+        },
+        {
+          logoName: 'Zlín cz',
+          logoImg: zlinImg
+        }
+      ]
     }
   }
 }
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -4,7 +4,7 @@
       <div class="quiz grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 grid-rows-1">
         <div class="quiz-card bg-white grid grid-cols-1 gap-y-32 p-4 h-80 mobile:h-96 sm:h-80 md:h-96">
           <p class="uppercase text-lososPink mb-4 text-xl font-bold">Zajímavé fakty</p>
-          <h3 class="uppercase xl:text-4xl lg:text-3xl md:text-4xl text-2xl font-heading mb-8 xl:leading-normal ">Rychlý kvíz<br>o festivalu culturea</h3>
+          <h3 class="uppercase xl:text-4xl lg:text-3xl md:text-4xl text-2xl font-heading mb-8 xl:leading-normal">Rychlý kvíz<br>o festivalu culturea</h3>
         </div>
         <QuizCard
           v-for="(quizCard, index) in quizCards"
